821e4c3808520b8112cc19b331d8f5f530afa675
yodapuuu <8720142@ed.tus.ac.jp>
2022-10-16 11:09:12 +0900

    'modelsを定義'

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class User(AbstractUser):
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=32, validators=[MinLengthValidator(8)])
 
 
 # class FriendShip(models.Model):
