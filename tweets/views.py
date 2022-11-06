8f4c90f2c347e06272dd4dcab8f1e6e44645d811
yodapuuu <8720142@ed.tus.ac.jp>
2022-11-06 14:06:49 +0900

    '2次レビュー修正'

--- tweets/views.py
+++ tweets/views.py
@@ -1,8 +1,5 @@
 from django.views.generic import TemplateView
 
-# from django.contrib.auth.mixins import LoginRequiredMixin
-
 
 class HomeView(TemplateView):
     template_name = "tweets/home.html"
-    login_url = "/login/"
