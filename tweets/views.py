33e33a6a8400181cd659cefce506251cf5c53911
yodapuuu <8720142@ed.tus.ac.jp>
2022-11-20 12:48:46 +0900

    '削除機能まで（本人以外も削除できちゃう状態）'

--- tweets/views.py
+++ tweets/views.py
@@ -1,23 +1,38 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+# from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
-
-from .forms import TweetForm
+from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic.edit import DeleteView
 from .models import TweetModel
+from .forms import TweetForm
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(LoginRequiredMixin, ListView):
     template_name = "tweets/home.html"
+    model = TweetModel
+    ordering = "-created_date"
 
 
-class TweetCreateView(CreateView):
+class TweetCreateView(LoginRequiredMixin, CreateView):
     template_name = "tweets/tweet.html"
     form_class = TweetForm
     model = TweetModel
     success_url = reverse_lazy("tweets:home")
 
     def form_valid(self, form):
-        self.tweet = form.save(commit=False)
-        self.tweet.author = self.request.user
-        self.tweet.save()
+        self.tweet_obj = form.save(commit=False)
+        self.tweet_obj.author = self.request.user
+        self.tweet_obj.save()
         return super().form_valid(form)
+
+
+class TweetDetailView(LoginRequiredMixin, DetailView):
+    template_name = "tweets/detail.html"
+    model = TweetModel
+
+
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = "tweets/delete.html"
+    model = TweetModel
+    success_url = reverse_lazy("tweets:home")
