c09b92555a40ac85ee7171d7916e0878aaa16363
yodapuuu <8720142@ed.tus.ac.jp>
2022-12-16 01:10:19 +0900

    'follow_listたちを書いた'

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.views import View
 from django.shortcuts import get_object_or_404, redirect
 from .forms import AccountsForm, LoginForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import User, Friendship
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -38,9 +38,56 @@ class FollowView(LoginRequiredMixin, View):
     http_method_names = ["post"]
 
     def post(self, request, *args, **kwargs):
-        followee = get_object_or_404(User, username=self.kwargs["username"])
-        follower = request.user
-        print(followee, follower)
-        print(kwargs)
 
-        return redirect("tweets:user_profile", followee.username)
+        self.followee = get_object_or_404(User, username=self.kwargs["username"])
+        self.follower = request.user
+
+        if self.followee == self.follower:
+            pass  # error_messagesを表示
+
+        else:
+            self.frienship = Friendship.objects.create(
+                followee=self.followee,
+                follower=self.follower,
+            )
+
+        return redirect("tweets:user_profile", self.frienship.followee.username)
+
+
+class UnFollowView(LoginRequiredMixin, View):
+
+    http_method_names = ["post"]
+
+    def post(self, request, *args, **kwargs):
+
+        self.followee = get_object_or_404(User, username=self.kwargs["username"])
+        self.follower = request.user
+
+        if self.followee == self.follower:
+            pass  # error_messagesを表示
+
+        else:
+            Friendship.objects.filter(
+                followee=self.followee,
+                follower=self.follower,
+            ).delete()
+
+        return redirect("tweets:user_profile", self.follower.username)
+
+
+class FollowingListView(ListView):
+    model = Friendship
+    template_name = "accounts/following_list.html"
+
+    def get_queryset(self):
+        self.follower = get_object_or_404(User, username=self.kwargs["username"])
+        return Friendship.objects.filter(follower=self.follower)
+
+
+class FollowerListView(ListView):
+    model = Friendship
+    template_name = "accounts/follower_list.html"
+
+    def get_queryset(self):
+        self.followee = get_object_or_404(User, username=self.kwargs["username"])
+        return Friendship.objects.filter(followee=self.followee)
